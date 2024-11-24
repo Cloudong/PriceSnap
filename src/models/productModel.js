a36bae010240e355ce78eefbf48486df9249e556
yeonjy73 <yeon703@naver.com>
2024-11-25 02:31:28 +0900

    fix: 상품 키워드 검색 수정

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,5 +1,5 @@
 const docClient = require('../config/dbConfig');
-const { GetCommand, PutCommand, QueryCommand } = require("@aws-sdk/lib-dynamodb");
+const { GetCommand, PutCommand, ScanCommand } = require("@aws-sdk/lib-dynamodb");
 
 const PRODUCTS_TABLE = process.env.PRODUCTS_TABLE; // 환경변수에서 테이블 이름 가져오기
   
@@ -24,26 +24,23 @@ const createProduct = async (product) => {
     await docClient.send(command);
 };
 
-// 제품 이름으로 제품 검색하는 함수
+// 상품 키워드 검색 함수
 const queryProductsByName = async (name) => {
   const params = {
     TableName: PRODUCTS_TABLE,
-    IndexName: 'product_name-index', // GSI 이름
-    KeyConditionExpression: 'product_name = :name', // 정확한 일치 검색
     FilterExpression: 'contains(product_name, :partialName)', // 부분 일치 검색
     ExpressionAttributeValues: {
-      ':name': name,           // 정확한 일치를 위한 값
-      ':partialName': name,    // 부분 일치를 위한 값
+      ':partialName': name, // 부분 일치를 위한 값
     },
   };
 
   try {
-    const command = new QueryCommand(params);
+    const command = new ScanCommand(params); // ScanCommand로 변경
     const result = await docClient.send(command);
     return result.Items; // 검색된 제품 반환
   } catch (error) {
-    console.error("Unable to query products. Error JSON:", JSON.stringify(error, null, 2));
-    throw new Error('Could not query products');
+    console.error("Unable to scan products. Error JSON:", JSON.stringify(error, null, 2));
+    throw new Error('Could not scan products');
   }
 };
 
