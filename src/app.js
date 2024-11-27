58b58faf513a5e198b01fd23b55173c4ba3231f7
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-11-27 15:52:21 +0900

    feat: 세선 기능 추가

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes"); // 라우트 임포트
-const productRoutes = require("./routes/productRoutes"); // 라우트 임포트
+const productRoute = require("./routes/productRoute");
+
+// CORS 설정 부분
+/*
+
+*/
+
+// 세션 설정
+app.use(
+    session({
+        secret: process.env.SESSION_KEY,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false }, // HTTPS 사용하나? 사용하면 true
+    })
+);
+
+// body-parser 설정
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// 테스트
+const publicDataService = require("./services/publicDataService");
+publicDataService.startFetch();
 
 const app = express();
 app.use(express.json());
 
 // 라우트 설정
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/products", productRoute);
 
-app.use("/api/test", async (req, res) => {
-    res.status(200).json({"message": "testMessage"});
+app.use("/test", async (req, res) => {
+    res.status(200).json({ message: "testMessage" });
 });
-  
+
 app.use((req, res) => {
-  return res.status(404).json({ error: "Not Found" });
+    return res.status(404).json({ error: "Not Found" });
 });
 
 module.exports = app;
