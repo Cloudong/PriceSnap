f050765d06a895bdf0ef32221ecabcd517dcde31
yeonjy73 <yeon703@naver.com>
2024-11-29 19:54:21 +0900

    fix: 장바구니 상품 추가 api endpoint 수정

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -4,6 +4,6 @@ const cartHandler = require("../handlers/cartHandler");
 const router = express.Router();
 
 // 장바구니에 상품 추가 API 라우트
-router.post("/", cartHandler.addProductToCartHandler);
+router.post("/items", cartHandler.addProductToCartHandler);
 
 module.exports = router;
