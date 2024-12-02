59b70c0d9b083b312f9aa2ebb7f3d377dc17fbe0
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-12-02 16:51:06 +0900

    feat: 공공데이터 Fetch 스케줄링 기능 추가

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+// 1달 간격으로 데이터 불러오기 위한 라이브러리
+const cron = require("node-cron");
+const { startFetch } = require("../src/services/publicDataService");
 
 // 라우트 임포트
 const userRoutes = require("./routes/userRoutes");
@@ -16,17 +19,17 @@ app.use(express.urlencoded({ extended: true }));
 // CORS 설정 부분
 const corsOptions = {
     origin: ["https://d6rlnxefknq73.cloudfront.net", "http://localhost:3000"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: 'Content-Type, Authorization, Set-Cookie',
-    credentials: true // 쿠키 및 인증 정보 포함
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: "Content-Type, Authorization, Set-Cookie",
+    credentials: true, // 쿠키 및 인증 정보 포함
 };
 app.use(cors(corsOptions));
 
 // OPTIONS 요청에 대한 응답 설정
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://d6rlnxefknq73.cloudfront.net');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Set-Cookie');
+app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "https://d6rlnxefknq73.cloudfront.net");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie");
     res.sendStatus(200);
 });
 
@@ -53,6 +56,17 @@ app.use("/api/budgets", budgetRoute);
 app.use((req, res, next) => {
     console.log(`Received ${req.method} request on ${req.url}`);
     next();
+});
+
+// 스케줄링 부분 (매달 10일 자정)
+cron.schedule("0 0 10 * *", async () => {
+    console.log("스케줄링 실행: startFetch 함수 호출");
+    try {
+        await startFetch();
+        console.log("startFetch 실행 완료");
+    } catch (error) {
+        console.error("startFetch 실행 중 오류 발생: ", error.message);
+    }
 });
 
 app.listen("4000", () => {
