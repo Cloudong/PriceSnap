9937f26f963756a400e4db998554bae9fe7bf4f6
yeonjy73 <yeon703@naver.com>
2024-12-02 16:24:44 +0900

    fix: 전주 -> 전전월로 수정

--- src/dtos/productDtos/productDTO.js
+++ src/dtos/productDtos/productDTO.js
@@ -1,10 +1,10 @@
 class ProductDTO {
-    constructor(productId, product_name, current_month_price, previous_month_price, previous_week_price) {
+    constructor(productId, product_name, current_month_price, previous_month_price, previous_two_months_price) {
       this.product_id = productId;
       this.product_name = product_name;
       this.current_month_price = current_month_price;
       this.previous_month_price = previous_month_price;
-      this.previous_week_price = previous_week_price;
+      this.previous_two_months_price = previous_two_months_price;
     }
   }
   
