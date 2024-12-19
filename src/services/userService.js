0c90505a9632f936851e7bcf5915788f2222983d
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2024-12-19 19:20:13 +0900

    chore: redeploy

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,20 @@
-const { getUserById, createUser, updateNameById } = require("../models/userModel");
+const {
+  getUserById,
+  createUser,
+  updateNameById,
+} = require("../models/userModel");
 
+//user 관련
 const fetchUser = async (userId) => {
-    return await getUserById(userId);
+  return await getUserById(userId);
 };
 
 const registerUser = async (userId, name, user_password) => {
-    await createUser(userId, name, user_password);
+  await createUser(userId, name, user_password);
 };
 
 const updateName = async (userId, newName) => {
-    await updateNameById(userId, newName);
+  await updateNameById(userId, newName);
 };
 
 module.exports = { fetchUser, registerUser, updateName };
